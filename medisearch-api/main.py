73645995842e60fe0c0fa8e2881d130c40095398
hakimamarullah <hakimamarullah@gmail.com>
2022-12-06 02:43:05 +0700

    remove unnecessary code

--- medisearch-api/main.py
+++ medisearch-api/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/search")
 async def search(q: str):
     start_time = time.process_time()
-    result = await Ranker.get_documents(query=q)
+    result = await Ranker.get_documents(query=q) if len(q) != 0 else []
     return {"query_time":time.process_time() - start_time,"message": "success", "data": result}
 
--- medisearch-api/utils/letor.py
+++ medisearch-api/utils/letor.py
@@ -1,14 +1,9 @@
-import random
 
 import numpy as np
-from gensim.models import LsiModel
-from gensim.corpora import Dictionary
 import pickle
 from scipy.spatial.distance import cosine
 
-import lightgbm
 import os
-from model.search_response import SearchResponse
 from .search_response_mapper import to_search_response
 
 current_dir =  os.path.abspath(os.path.dirname(__file__))
@@ -17,14 +12,16 @@ CORPUS_PATH = os.path.join(parent_dir, 'data', 'nfcorpus')
 
 
 
+def pickle_load_utils_trained(filename):
+   with open(os.path.join(os.getcwd(), 'utils','trained_model',filename), 'rb') \
+            as docs:
+            return pickle.load(docs)
 
 
 class CorpusDocuments(object):
 
-    def __init__(self, path: str) -> None: 
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','documents.docs'), 'rb') \
-            as docs:
-            [self.documents] = pickle.load(docs)
+    def __init__(self) -> None: 
+        [self.documents] = pickle_load_utils_trained('documents.docs')
     
     def __str__(self) -> str:
         """
@@ -34,11 +31,8 @@ class CorpusDocuments(object):
 
 class CorpusQuery(object):
 
-    def __init__(self, path:str) -> None:
-        
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','queries.queries'), 'rb') \
-            as queries:
-            [self.queries] = pickle.load(queries)
+    def __init__(self) -> None:
+        [self.queries] = pickle_load_utils_trained('queries.queries')
     
     def __str__(self) -> str:
         """
@@ -46,71 +40,49 @@ class CorpusQuery(object):
         """
         return str(list(self.queries.items())[0])
 
-class Model(CorpusDocuments, CorpusQuery):
+class PretrainedModel(CorpusDocuments, CorpusQuery):
     NUM_NEGATIVES = 1
     NUM_LATENT_TOPICS = 200
 
-    def __init__(self, docs_path: str = 'train.docs', query_path:str = 'train.vid-desc.queries', qrel_path:str = 'train.3-2-1.qrel') -> None:
+    def __init__(self) -> None:
         print("Building The Model....")
-        CorpusDocuments.__init__(self,docs_path)
-        CorpusQuery.__init__(self,query_path)
+        CorpusDocuments.__init__(self)
+        CorpusQuery.__init__(self)
         self.q_docs_rel = dict()
         self.group_qid_count = list()
         self.dataset = list()
         self.docs_vector = dict()
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','docs-vetor.vector'), 'rb') as vectors:
-           [self.docs_vector] = pickle.load(vectors)
-        self.init_q_docs_rel(qrel_path)
+        
+        [self.docs_vector] = pickle_load_utils_trained('docs-vetor.vector')
+        self.init_q_docs_rel()
         self.load_dataset()
     
-    def save_lsa(self):
-        with open(os.path.join(os.getcwd(), 'trained_model','lsa.pkl'), 'wb+') as lsa:
-            pickle.dump([self.lsi_model], lsa)
-        lsa.close()
     
     def load_lsa(self):
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','lsa.pkl'), 'rb') as lsa:
-            [self.lsi_model] = pickle.load(lsa)
-        lsa.close()
-    
-    def save_model(self):
-        with open(os.path.join(os.getcwd(), 'trained_model', 'model.pkl'), 'wb+') as model:
-            pickle.dump([self.ranker], model)
-        model.close()
-    
+        [self.lsi_model] = pickle_load_utils_trained('lsa.pkl')
+       
+  
     def load_model(self):
-        with open(os.path.join(os.getcwd(), 'utils', 'trained_model', 'model.pkl'), 'rb') as model:
-            [self.ranker] = pickle.load(model)
-        model.close()
+        [self.ranker] = pickle_load_utils_trained('model.pkl')
     
-    def init_q_docs_rel(self, qrel_path:str) -> None:
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','model.qrel'), 'rb') as qrel:
-            [self.q_docs_rel] = pickle.load(qrel)
+    def init_q_docs_rel(self) -> None:
+        [self.q_docs_rel] = pickle_load_utils_trained('model.qrel')
         
     
     def load_dataset(self) -> None:
+        [self.group_qid_count] = pickle_load_utils_trained('group-qid-count.data')
         
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','group-qid-count.data'), 'rb') as group:
-            [self.group_qid_count] = pickle.load(group)
-        
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','dataset.data'), 'rb') as data:
-            [self.dataset] = pickle.load(data)
+        [self.dataset] = pickle_load_utils_trained('dataset.data')
 
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','dictionary.dict'), 'rb') as dicts:
-            [self.dictionary] = pickle.load(dicts)
         
-        with open(os.path.join(os.getcwd(), 'utils','trained_model','bow-corpus.corpus'), 'rb') as bow:
-            [self.bow_corpus] = pickle.load(bow)
+        [self.dictionary] = pickle_load_utils_trained('dictionary.dict')
+
+
+        [self.bow_corpus] = pickle_load_utils_trained('bow-corpus.corpus')
 
         assert sum(self.group_qid_count) == len(self.dataset), "Ooops...Something's wrong!!"
         print("Dataset loaded successfully!")
     
-    def generate_lsa(self):
-        assert self.bow_corpus != None, "Run load_dataset() before generating LSA Model"
-        print("Generate LSA model..")
-        self.lsi_model = LsiModel(self.bow_corpus, num_topics = self.NUM_LATENT_TOPICS)
-        self.save_lsa()
-        print("LSA Model generated..")
     
     # test melihat representasi vector dari sebuah dokumen & query
     def vector_rep(self, text):
@@ -142,26 +114,7 @@ class Model(CorpusDocuments, CorpusQuery):
         print(Y.shape)
 
         return X, Y
-    
-    def train_model(self, objective="lambdarank", boosting_type="gbdt",
-     importance_type="gain", metric="ndcg", verbose=10):
-
-        X, Y = self.get_dataset()
-        self.ranker = lightgbm.LGBMRanker(
-                    objective=objective,
-                    boosting_type = boosting_type,
-                    n_estimators = 100,
-                    importance_type = importance_type,
-                    metric = metric,
-                    num_leaves = 40,
-                    learning_rate = 0.02,
-                    max_depth = -1)
-        # di contoh kali ini, kita tidak menggunakan validation set
-        # jika ada yang ingin menggunakan validation set, silakan saja
-        self.ranker.fit(X, Y,
-                group = self.group_qid_count,
-                verbose = verbose)
-        self.save_model()
+         
 
     def get_documents(self, query:str) -> list:
         self.load_lsa()
@@ -189,31 +142,12 @@ class Model(CorpusDocuments, CorpusQuery):
         {CorpusDocuments.__str__(self)} 
         {CorpusQuery.__str__(self)}'''
 
-class Ranker(Model):
+class Ranker(PretrainedModel):
 
     @staticmethod
-    def get_documents(query: str) -> list:
-        model = Model()
+    async def get_documents(query: str) -> list:
+        model = PretrainedModel()
         return model.get_documents(query)[:2]
     
 
-def main():
-  
-    query = "how much cancer risk can be avoided through lifestyle change ?"
-
-    docs =[("D1", "dietary restriction reduces insulin-like growth factor levels modulates apoptosis cell proliferation tumor progression num defici pubmed ncbi abstract diet contributes one-third cancer deaths western world factors diet influence cancer elucidated reduction caloric intake dramatically slows cancer progression rodents major contribution dietary effects cancer insulin-like growth factor igf-i lowered dietary restriction dr humans rats igf-i modulates cell proliferation apoptosis tumorigenesis mechanisms protective effects dr depend reduction multifaceted growth factor test hypothesis igf-i restored dr ascertain lowering igf-i central slowing bladder cancer progression dr heterozygous num deficient mice received bladder carcinogen p-cresidine induce preneoplasia confirmation bladder urothelial preneoplasia mice divided groups ad libitum num dr num dr igf-i igf-i/dr serum igf-i lowered num dr completely restored igf-i/dr-treated mice recombinant igf-i administered osmotic minipumps tumor progression decreased dr restoration igf-i serum levels dr-treated mice increased stage cancers igf-i modulated tumor progression independent body weight rates apoptosis preneoplastic lesions num times higher dr-treated mice compared igf/dr ad libitum-treated mice administration igf-i dr-treated mice stimulated cell proliferation num fold hyperplastic foci conclusion dr lowered igf-i levels favoring apoptosis cell proliferation ultimately slowing tumor progression mechanistic study demonstrating igf-i supplementation abrogates protective effect dr neoplastic progression"), 
-       ("D2", "study hard as your blood boils"), 
-       ("D3", "processed meats risk childhood leukemia california usa pubmed ncbi abstract relation intake food items thought precursors inhibitors n-nitroso compounds noc risk leukemia investigated case-control study children birth age num years los angeles county california united states cases ascertained population-based tumor registry num num controls drawn friends random-digit dialing interviews obtained num cases num controls food items principal interest breakfast meats bacon sausage ham luncheon meats salami pastrami lunch meat corned beef bologna hot dogs oranges orange juice grapefruit grapefruit juice asked intake apples apple juice regular charcoal broiled meats milk coffee coke cola drinks usual consumption frequencies determined parents child risks adjusted risk factors persistent significant associations children's intake hot dogs odds ratio num num percent confidence interval ci num num num hot dogs month trend num fathers intake hot dogs num ci num num highest intake category trend num evidence fruit intake provided protection results compatible experimental animal literature hypothesis human noc intake leukemia risk potential biases data study hypothesis focused comprehensive epidemiologic studies warranted"), 
-       ("D4", "long-term effects calorie protein restriction serum igf num igfbp num concentration humans summary reduced function mutations insulin/igf-i signaling pathway increase maximal lifespan health span species calorie restriction cr decreases serum igf num concentration num protects cancer slows aging rodents long-term effects cr adequate nutrition circulating igf num levels humans unknown report data long-term cr studies num num years showing severe cr malnutrition change igf num igf num igfbp num ratio levels humans contrast total free igf num concentrations significantly lower moderately protein-restricted individuals reducing protein intake average num kg num body weight day num kg num body weight day num weeks volunteers practicing cr resulted reduction serum igf num num ng ml num num ng ml num findings demonstrate unlike rodents long-term severe cr reduce serum igf num concentration igf num igfbp num ratio humans addition data provide evidence protein intake key determinant circulating igf num levels humans suggest reduced protein intake important component anticancer anti-aging dietary interventions"), 
-       ("D5", "cancer preventable disease requires major lifestyle abstract year num million americans num million people worldwide expected diagnosed cancer disease commonly believed preventable num num cancer cases attributed genetic defects remaining num num roots environment lifestyle lifestyle factors include cigarette smoking diet fried foods red meat alcohol sun exposure environmental pollutants infections stress obesity physical inactivity evidence cancer-related deaths num num due tobacco num num linked diet num num due infections remaining percentage due factors radiation stress physical activity environmental pollutants cancer prevention requires smoking cessation increased ingestion fruits vegetables moderate alcohol caloric restriction exercise avoidance direct exposure sunlight minimal meat consumption grains vaccinations regular check-ups review present evidence inflammation link agents/factors cancer agents prevent addition provide evidence cancer preventable disease requires major lifestyle")]
-    
-    model = Model("train.docs", "train.vid-desc.queries", "train.3-2-1.qrel")
-    model.generate_lsa()
-    model.train_model()
-    scores = model.predict(docs, query)
-
-    model.interpret_serp_ranking(docs, query, scores)
-
-if __name__ == '__main__':
-    main()
 
